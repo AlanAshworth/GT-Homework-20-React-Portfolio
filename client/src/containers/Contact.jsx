31e51948152bed0c88af11e8888bdd8dc732fb2d
Alan Ashworth <awashworth927@gmail.com>
2020-05-06 18:29:24 -0400

    Update Contact.jsx 'for' attribute to 'htmlFor' to React DOM API.

--- client/src/containers/Contact.jsx
+++ client/src/containers/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
               <h1 className="block-header">Contact</h1>
               <hr />
               <div className="form-group">
-                <label for="name" className="col-sm-2 control-label">
+                <label htmlFor="name" className="col-sm-2 control-label">
                   Name
                 </label>
                 <div className="col-sm-10">
@@ -23,7 +23,7 @@ const Contact = () => {
                 </div>
               </div>
               <div className="form-group">
-                <label for="email" className="col-sm-2 control-label">
+                <label htmlFor="email" className="col-sm-2 control-label">
                   Email
                 </label>
                 <div className="col-sm-10">
@@ -36,7 +36,7 @@ const Contact = () => {
                 </div>
               </div>
               <div className="form-group">
-                <label className="col-sm-2 control-label" for="msg">
+                <label className="col-sm-2 control-label" htmlFor="msg">
                   Message
                 </label>
                 <div className="col-sm-10">
