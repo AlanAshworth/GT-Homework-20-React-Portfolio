b8c17327285ceadaa21153d1e807d98047ab08c5
Alan Ashworth <awashworth927@gmail.com>
2020-05-06 21:13:55 -0400

    Flesh out header component. To add props to containers.

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
-const Header = () => {
-    return (
-        <header>
-            
-        </header>
-    );
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className="row">
+        <div className="col">
+          <h1>{props.title}</h1>
+          <p>{props.subtitle}</p>
+        </div>
+      </header>
+    </Fragment>
+  );
 };
 
 export default Header;
