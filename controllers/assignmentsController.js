1669a984ed1792ca761f3f78eb89ca889197537f
Alan Ashworth <awashworth927@gmail.com>
2020-07-16 09:47:43 -0400

    Small comment modification to reflect actual routing redirects.

--- controllers/assignmentsController.js
+++ controllers/assignmentsController.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-// Defining methods for the championsController
+// Defining methods for the assignmentsController
 module.exports = {
   findAll: function(req, res) {
     db.Portfolio
--- routes/api/assignments.js
+++ routes/api/assignments.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const assignmentsController = require("../../controllers/assignmentsController");
 
-// Matches with "/api/champions"
+// Matches with "/api/assignments"
 router.route("/")
   .get(assignmentsController.findAll)
   .post(assignmentsController.create);
 
-// Matches with "/api/champions/:id"
+// Matches with "/api/assignments/:id"
 router
   .route("/:id")
   .get(assignmentsController.findById)
